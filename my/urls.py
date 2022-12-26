3b67392c4b87f2577a35604b0caba33440622b56
Rohit <allrd.rohit@gmail.com>
2022-12-26 17:08:52 +0000

    Remove from commit

--- my/urls.py
+++ my/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('checkout/',views.checkout,name='checkout')
 ]
 
-# for media
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
